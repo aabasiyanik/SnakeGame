a51d0900a7df916f00574c34de3df4b471a06585
Ahmed Abasiyanik <ahmedabasiyanik03@gmail.com>
2023-03-17 22:58:34 -0500

    Add apple.png, adjust the position of the apple

--- snake.py
+++ snake.py
@@ -17,8 +17,16 @@ initial_head_pos = [CELL_SIZE * 3, CELL_SIZE * 3]
 snake_pos = [initial_head_pos, [initial_head_pos[0] - CELL_SIZE, initial_head_pos[1]], [initial_head_pos[0] - 2 * CELL_SIZE, initial_head_pos[1]]]
 
 snake_speed = [CELL_SIZE, 0]
-food_pos = [random.randrange(0, (WIDTH // CELL_SIZE)) * CELL_SIZE, random.randrange(0, (HEIGHT // CELL_SIZE)) * CELL_SIZE]
 
+def generate_food_pos():
+    while True:
+        food_x = random.randrange(1, (WIDTH // CELL_SIZE)) * CELL_SIZE
+        food_y = random.randrange(1, (HEIGHT // CELL_SIZE)) * CELL_SIZE
+        new_food_pos = [food_x, food_y]
+        if new_food_pos not in snake_pos:
+            return new_food_pos
+
+food_pos = generate_food_pos()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Snake Game')
@@ -27,9 +35,12 @@ clock = pygame.time.Clock()
 score = 0
 font = pygame.font.SysFont(None, 30)
 
+apple_image = pygame.image.load('apple.png')
+apple_image = pygame.transform.scale(apple_image, (CELL_SIZE, CELL_SIZE))
+
 def draw_grid():
     for x in range(0, WIDTH, CELL_SIZE):
-        pygame.draw.line(screen, ( 150, 191, 13), (x, 0), (x, HEIGHT))
+        pygame.draw.line(screen, (150, 191, 13), (x, 0), (x, HEIGHT))
     for y in range(0, HEIGHT, CELL_SIZE):
         pygame.draw.line(screen, (150, 191, 13), (0, y), (WIDTH, y))
 
@@ -57,7 +68,7 @@ def draw_objects():
         pygame.draw.rect(screen, (98, 189, 105), pygame.Rect(pos[0], pos[1], CELL_SIZE, CELL_SIZE))
         pygame.draw.rect(screen, GRAY, pygame.Rect(pos[0], pos[1], CELL_SIZE, CELL_SIZE), 1)
 
-    pygame.draw.rect(screen, RED, pygame.Rect(food_pos[0], food_pos[1], CELL_SIZE, CELL_SIZE))
+    screen.blit(apple_image, (food_pos[0], food_pos[1]))
     score_text = font.render("Score: " + str(score), True, (0, 0, 0))
     screen.blit(score_text, (10, 10))
     draw_grid()
@@ -74,14 +85,12 @@ while True:
             if event.key == pygame.K_RIGHT and snake_speed[0] != -CELL_SIZE:
                 snake_speed = [CELL_SIZE, 0]
 
-
-
         if event.type == pygame.QUIT:
             sys.exit()
 
     snake_pos.insert(0, [round(snake_pos[0][0] + snake_speed[0]), round(snake_pos[0][1] + snake_speed[1])])
     if snake_pos[0] == food_pos:
-        food_pos = [random.randrange(1, (WIDTH//CELL_SIZE)) * CELL_SIZE, random.randrange(1, (HEIGHT//CELL_SIZE)) * CELL_SIZE]
+        food_pos = generate_food_pos()
         score += 1
     else:
         snake_pos.pop()
@@ -92,7 +101,7 @@ while True:
                 initial_head_pos = [CELL_SIZE * 3, CELL_SIZE * 3]
                 snake_pos = [initial_head_pos, [initial_head_pos[0] - CELL_SIZE, initial_head_pos[1]], [initial_head_pos[0] - 2 * CELL_SIZE, initial_head_pos[1]]]
                 snake_speed = [CELL_SIZE, 0]
-                food_pos = [random.randrange(0, (WIDTH//CELL_SIZE)) * CELL_SIZE, random.randrange(0, (HEIGHT//CELL_SIZE)) * CELL_SIZE]
+                food_pos = generate_food_pos()
                 score = 0
             else:
                 sys.exit()
@@ -103,10 +112,11 @@ while True:
             initial_head_pos = [CELL_SIZE * 3, CELL_SIZE * 3]
             snake_pos = [initial_head_pos, [initial_head_pos[0] - CELL_SIZE, initial_head_pos[1]], [initial_head_pos[0] - 2 * CELL_SIZE, initial_head_pos[1]]]
             snake_speed = [CELL_SIZE, 0]
-            food_pos = [random.randrange(0, (WIDTH//CELL_SIZE)) * CELL_SIZE, random.randrange(0, (HEIGHT//CELL_SIZE)) * CELL_SIZE]
+            food_pos = generate_food_pos()
             score = 0
         else:
             sys.exit()
+
     draw_objects()
     pygame.display.update()
     clock.tick(20)
